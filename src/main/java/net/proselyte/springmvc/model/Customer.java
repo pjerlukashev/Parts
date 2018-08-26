6f0031acdc6f6a839979c6122241c7ec422a6c54
Petr Lukashev <lukashev_pjer@mail.ru>
2018-08-26 15:01:20 +0300

    Strange stuff with workplace.xml

--- src/main/java/net/proselyte/springmvc/model/Customer.java
+++ src/main/java/net/proselyte/springmvc/model/Customer.java
@@ -1,4 +1,36 @@
 package net.proselyte.springmvc.model;
 
+import java.util.Set;
+
 public class Customer {
+
+    private String name;
+    private Set projects;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set projects) {
+        this.projects = projects;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", projects=" + projects +
+                '}';
+    }
+
+
+
 }
